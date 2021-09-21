38ade410dbadbdb754aaea59249532fe7bdfb3a8
sugam <sugamkarki7058@gmail.com>
2021-09-21 08:43:09 +0545

    added ts-node-dev and made basic mutation for register and query

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { buildSchema, Query, Resolver } from "type-graphql";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 //
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello world!!!";
-  }
-}
+import { RegisterResolver } from "./modules/user/Register";
+//
+
 //
 const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [HelloResolver],
-  });
-  const apolloServer = new ApolloServer({ schema });
-  const app = express();
-  await apolloServer.start();
-  apolloServer.applyMiddleware({ app });
-  app.listen(4000, () => {
-    console.log("server started  in http://localhost:4000");
-  });
+  try {
+    await createConnection();
+    const schema = await buildSchema({
+      resolvers: [RegisterResolver],
+    });
+    const apolloServer = new ApolloServer({ schema });
+    const app = express();
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+      console.log("server started  in http://localhost:4000/graphql");
+    });
+  } catch (err) {
+    console.log("error", err);
+  }
 };
 main();
---
+++ src/modules/user/Register.ts
@@ -0,0 +1,29 @@
+import { Resolver, Mutation, Query, Arg } from "type-graphql";
+import bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
+@Resolver()
+export class RegisterResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello world!!!";
+  }
+  //
+
+  //
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save();
+    return user;
+  }
+}
